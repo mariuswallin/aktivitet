8859caf6c940d4b27ac0bac8983a82a2de3933e1
Marius Johansen <post@mariuswallin.no>
2016-05-17 22:05:09 +0200

    first commit

---
+++ akt/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mariusjohansen/Dropbox/LearnDjangoTo/aktivitetsportal/akt/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
